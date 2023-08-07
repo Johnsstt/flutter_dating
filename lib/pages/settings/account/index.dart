d9db0f0134af843348c8ce133db1953d4720647e
slei <john_tech@163.com>
2023-08-07 14:50:39 +0800

    feat: flutter plugins

---
+++ lib/pages/settings/account/index.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dating_template/wcao/ui/cell.dart';
+import 'package:get/get.dart';
+
+/// HACK: 账户设置
+
+class SettingsAccount extends StatefulWidget {
+  const SettingsAccount({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsAccount> createState() => _SettingsAccountState();
+}
+
+class _SettingsAccountState extends State<SettingsAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('账号设置'),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Cell('修改密码', onTap: () => Get.toNamed('/password/update')),
+            Cell('更换绑定手机号',
+                onTap: () => Get.toNamed('/settings/account/update-phone')),
+            Cell('注销账号', onTap: () => Get.toNamed('/settings/account/logout')),
+          ],
+        ),
+      ),
+    );
+  }
+}
